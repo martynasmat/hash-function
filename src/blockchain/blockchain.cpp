f39858aae936efab4cc28e1e3f7fe39087b404fa
martynasmat <martynasmat03@gmail.com>
2025-10-29 20:33:39 +0200

    feat: printChain func

--- src/blockchain/blockchain.cpp
+++ src/blockchain/blockchain.cpp
@@ -27,3 +27,47 @@ void Blockchain::genBlock(const std::vector<Transaction>& txs) {
     head = node;
     block_count += 1;
 }
+
+void Blockchain::printChain() const {
+    cout << "\nBLOCKCHAIN [last block -> genesis block]\n";
+
+    const BlockNode* cur = head;
+    size_t index = 0;
+
+    while (cur != nullptr) {
+        const Block& block = cur->block;
+
+        cout << "Block no.:"
+                  << (block_count - 1 - index) << "\n";
+
+        cout << "  Block Hash:   "
+                  << Block::toHex(block.getHash()) << "\n";
+
+        cout << "  Prev Block Hash:    "
+                  << Block::toHex(block.getPrevHash()) << "\n";
+
+        cout << "  Nonce:        "
+                  << block.getNonce() << "\n";
+
+        cout << "  Tx Count:     "
+                  << block.getTransactions().size() << "\n";
+
+        if (cur->prev_hp.prev_block_ptr == nullptr) {
+            // Genesis block
+            cout << "  HashPointer.prev_block_ptr:  nullptr\n";
+            cout << "  HashPointer.prev_block_hash: "
+                      << Block::toHex(cur->prev_hp.prev_block_hash)
+                      << "\n";
+        } else {
+            cout << "  HashPointer.prev_block_ptr:  "
+                      << cur->prev_hp.prev_block_ptr << "\n";
+
+            cout << "  HashPointer.prev_block_hash: "
+                      << Block::toHex(cur->prev_hp.prev_block_hash)
+                      << "\n";
+        }
+
+        cur = cur->prev_hp.prev_block_ptr;
+        index++;
+    }
+}
