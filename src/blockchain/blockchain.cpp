148d5b6b009bf19f94ab5d244f97cae85b27cf25
martynasmat <martynasmat03@gmail.com>
2025-10-29 20:29:23 +0200

    feat: genBlock func

--- src/blockchain/blockchain.cpp
+++ src/blockchain/blockchain.cpp
@@ -13,3 +13,17 @@ void Blockchain::genGenesisBlock(const vector<Transaction>& txs) {
     head = new BlockNode(std::move(genesis));
     block_count = 1;
 }
+
+void Blockchain::genBlock(const std::vector<Transaction>& txs) {
+    std::array<uint8_t,16> prev_hash = head->block.getHash();
+
+    Block newBlock(prev_hash, txs, difficulty);
+    newBlock.mine();
+
+    HashPointer hp(prev_hash, head);
+
+    auto* node = new BlockNode(std::move(newBlock), hp);
+
+    head = node;
+    block_count += 1;
+}
